22570db96d776b317ede51417d96e8b604e9e8d5
Ardi <ardiantosaputro2001@gmail.com>
2023-10-27 14:41:20 +0700

    Functionality tipe 24 Jam Siap

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const semiColon = ":";
-let date = new Date();
 
-let timeHour = String(date.getHours()).padStart(2, "0")
-let timeMinutes = String(date.getMinutes()).padStart(2, "0")
-let timeSeconds = String(date.getSeconds()).padStart(2, "0")
+window.setInterval(waktuSekarang, 1000);
 
-let waktu = timeHour + ":" + timeMinutes + ":" + timeSeconds;
+function waktuSekarang() {
+  let date = new Date();
 
-document.getElementById("waktuSekarang").innerText = waktu;
+  let timeHour = String(date.getHours()).padStart(2, "0")
+  let timeMinutes = String(date.getMinutes()).padStart(2, "0")
+  let timeSeconds = String(date.getSeconds()).padStart(2, "0")
 
-// window.setInterval(update, 1000);
+  let waktu = timeHour + ":" + timeMinutes + ":" + timeSeconds;
+
+  document.getElementById("waktuSekarang").innerText = waktu;
+}
+
+
 
 /*
 * Functionality First, Style Later
@@ -17,8 +21,8 @@ A. Functionality
     1. get time from local machine VVVVVVV
     2. display time in two digits format VVVV
       3. append ":" between hour, minute and seconds VVVV Bug, malahan jadi terus tersambung
-    3. cara menampilkan, i dont know
-    4. function update?
+    3. cara menampilkan di browser VVVV
+    4. setInterval untuk update waktu
 
 B. Style
     1. Honestly, still not have idea.
